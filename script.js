a281fe93f1641e56a7d4404e243db02992c08127
Dzmitrysiniakou <jack.pott@mail.ru>
2020-03-12 13:14:15 +0300

    feat: second slide done, add change background in js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function changeSlide(id){
    document.getElementById((id == "block-one-phone") ? "block-two-phone": "block-one-phone").style.display = "none";
-   document.getElementById((id == "block-one-phone") ? "block-one-phone": "block-two-phone").style.display = "block";
+   document.getElementById((id == "block-one-phone") ? "block-one-phone": "block-two-phone").style.display = "flex";
+   document.getElementById("block-one-phone").style.background = "#648BF0";
 }
